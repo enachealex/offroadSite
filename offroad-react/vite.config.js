ad4441f4537597e0625c836332744fb5b5e87ce3
enachealex <enachealex1@gmail.com>
2026-03-22 15:29:55 -0700

    auto deploy

--- offroad-react/vite.config.js
+++ offroad-react/vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// Set the base path for GitHub Pages deployment
+const repoName = 'offroadSite'; // Change if your repo name is different
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: `/${repoName}/`,
   plugins: [react(), tailwindcss()],
 })
